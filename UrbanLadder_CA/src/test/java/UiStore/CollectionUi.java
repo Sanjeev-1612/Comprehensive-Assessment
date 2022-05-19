c15baf825dbc20145f984b68ae0e564ad73dc179
Sanjeev S <sanjkuttan16@gmail.com>
2022-05-19 09:45:15 +0530

    Adding project

---
+++ UrbanLadder_CA/src/test/java/UiStore/CollectionUi.java
@@ -0,0 +1,12 @@
+package UiStore;
+
+import org.openqa.selenium.By;
+
+public class CollectionUi {
+
+	public static By coll=By.xpath("//li[@class='topnav_item collectionsunit']");
+	public static By ul=By.xpath("//a[@href='/ul-exclusive?src=g_topnav_collections_ul-exclusive']");
+
+}
+
+
---
+++ UrbanLadder_CA/src/test/java/UiStore/KidsUi.java
@@ -0,0 +1,9 @@
+package UiStore;
+
+import org.openqa.selenium.By;
+
+public class KidsUi {
+
+	public static By kid =By.xpath("//li[@class='topnav_item kidsroomunit']");
+	public static By play=By.xpath("//a[@href='/kids-play-table?src=g_topnav_kids-room_kids-tables_kids-play-tables']");
+}
